9d19560ad2d454dc533f93cfb4f4e8ec4d079008
fgjorgevski <74385707+fgjorgevski@users.noreply.github.com>
2021-04-06 19:55:17 +0200

    Exercise 1 done

---
+++ Class06_Recap/Exercise1/MyClasses/Human.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise1.MyClasses
+{
+    public class Human
+    {
+        public string FirstName{ get; set; }
+        public string LastName{ get; set; }
+        public int Age{ get; set; }
+
+        public Human(string name, string lastName, int age)
+        {
+            FirstName = name;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string GetPersonStats()
+        {
+
+            return $"{FirstName}: {LastName}: {Age}:";
+        }
+    }
+}
--- Class06_Recap/Exercise1/Program.cs
+++ Class06_Recap/Exercise1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exercise1.MyClasses;
+using System;
 
 namespace Exercise1
 {
@@ -6,7 +7,26 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter Name,last name, age");
+            string name = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int checkedAge;
+            bool age = int.TryParse(Console.ReadLine(), out checkedAge);
+            if (age)
+            {
+                Human human1 = new Human(name, lastName, checkedAge);
+
+                Console.WriteLine(human1.GetPersonStats());
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+
+
+            
+
         }
     }
 }
