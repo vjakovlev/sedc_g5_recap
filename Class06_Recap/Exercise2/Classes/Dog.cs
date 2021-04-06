ab2ac6177537183c8233de8995ac8d59952148c7
Nikola Nikolov <nikola.nikolov96@outlook.com>
2021-04-06 20:11:10 +0200

    Exercise02 added

---
+++ Class06_Recap/Exercise2/Classes/Dog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise2.Classes
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string Color { get; set; }
+        public string Eat()
+        {
+            string message = $"{Name} the dog from the breed {Race} whose fur is {Color} is now eating. Nummyy nummy :)";
+            return message;
+        }
+        public string Play()
+        {
+            string message = $"{Name} the dog from the breed {Race} whose fur is {Color} is now playing with his toys \nand {Name} is making \"ROAR ROAR\"!";
+            return message;
+        }
+        public string ChaseTail()
+        {
+            string message = $"{Name} the dog from the breed {Race} whose fur is {Color} is now spinning around and chasing it's tail!";
+            return message;
+        }
+    }
+}
---
+++ Class06_Recap/Exercise2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Exercise2.Classes;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter dog's name: ");
+            string dogName = Console.ReadLine();
+
+            Console.Write("Enter dog's race: ");
+            string dogRace = Console.ReadLine();
+
+            Console.Write("Enter dog's color: ");
+            string dogColor = Console.ReadLine();
+
+            Dog dog01 = new Dog()
+            {
+                Name = dogName,
+                Race = dogRace,
+                Color = dogColor
+            };
+
+            while (true)
+            {
+                Console.WriteLine("Choose one of the actions, you want your dog to do:" +
+                                "\nIf you want the dog to eat enter - eat" +
+                                "\nIf you want the dog to play enter - play" +
+                                "\nIf you want the dog to chase it's tail write - chase tail" +
+                                "\nIf you want to quit enter - quit");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "eat":
+                        Console.WriteLine(dog01.Eat());
+                        break;
+                    case "play":
+                        Console.WriteLine(dog01.Play());
+                        break;
+                    case "chase tail":
+                        Console.WriteLine(dog01.ChaseTail());
+                        break;
+                    case "quit":
+                        return;
+                    default:
+                        Console.WriteLine("Please enter one of the options written below!!!");
+                        break;
+                }
+            }
+        }
+    }
+}
