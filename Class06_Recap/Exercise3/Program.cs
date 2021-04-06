057e27a28ef22bd98a7b80ea69473c4e339d88a7
anastas <anastasgeshovski@gmail.com>
2021-04-06 21:03:56 +0200

    Class06_Recap

---
+++ Class06_Recap/Exercise3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student marko = new Student("Marko", "SEDC", "G1");
+            Student maja = new Student("Maja", "SEDC", "G2");
+            Student ivo = new Student("Ivo", "SEDC", "G3");
+            Student angela = new Student("Angela", "SEDC", "G1");
+            Student slavko = new Student("Slavko", "SEDC", "G1");
+
+            Student[] studentList = new Student[5];
+            studentList[0] = angela;
+            studentList[1] = ivo;
+            studentList[3] = maja;
+            studentList[2] = slavko;
+            studentList[4] = marko;
+
+            string inputName = Console.ReadLine();
+            foreach (var student in studentList)
+            {
+                if (student.Name.ToLower() == inputName.ToLower())
+                {
+                    Console.WriteLine($"{student.Name}, {student.Group}, {student.Academy}");
+                    return;
+                } 
+
+            }
+            Console.WriteLine("Error!");
+        }
+    }
+}
---
+++ Class06_Recap/Exercise3/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise3
+{
+    public class Student 
+    {
+        public string Name { get; set; }
+        public string Academy { get; set; }
+        public string Group { get; set; }
+
+
+        public Student(string name, string academy, string group)
+        {
+            Name = name;
+            Academy = academy;
+            Group = group;
+        }
+
+    }
+}
